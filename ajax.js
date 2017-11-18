99c696d5bf6b7950e37be40b3c5dda927881f1bf
tracykaur <tracykaur@yahoo.com>
2017-11-18 13:20:07 +1000

    ajaxExample2

--- ajax.js
+++ ajax.js
@@ -1,8 +1,7 @@
-var ajaxRequest = new XMLHttpRequest();
-ajaxRequest.addEventListener('readystatechange', function(r) {
-  if(r.target.status === 200) {
-    console.log(r.target.response);
-  }
-});
-ajaxRequest.open("GET", "https://api.github.com/users/cassidoo", true);
-ajaxRequest.send();
+fetch("https://api.github.com/users/tracykaur")
+  .then(function(r) {
+    return r.json();
+  })
+  .then(function(j) {
+    console.log(j);
+  })
